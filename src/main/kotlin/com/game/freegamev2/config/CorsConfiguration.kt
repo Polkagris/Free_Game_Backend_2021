161f40eea284b40bf15c0e70874d904f18ba52e4
unknown <arne.n.bjerkaas@gmail.com>
2021-01-26 22:44:31 +0100

    CORS config test

--- src/main/kotlin/com/game/freegamev2/config/CorsConfiguration.kt
+++ src/main/kotlin/com/game/freegamev2/config/CorsConfiguration.kt
@@ -1,25 +1,18 @@
 package com.game.freegamev2.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-
 @Configuration
-@EnableWebMvc
-class CorsConfiguration : WebMvcConfigurer {
-    override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://free-game-really.herokuapp.com/api");
-    } /* @Bean
-    public WebMvcConfigurer corsConfigurer()
-    {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/ **");
+class CorsConfiguration {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**").allowedOrigins("*")
             }
-        };
-    }*/
+        }
+    }
 }
